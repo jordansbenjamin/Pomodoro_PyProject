ccaa264392b88d5036923716c1163854258af9f3
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-18 17:27:45 +1000

    Added initial UI setup with pomodoro image, label and button

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Timer label
+timer_label = Label(text="Timer", font=(FONT_NAME, 55, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
+# Tomato canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+# canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Start button
+start_btn = Button(text="Start", highlightbackground=YELLOW)
+start_btn.grid(column=0, row=2)
+
+# Reset button
+reset_btn = Button(text="Reset", highlightbackground=YELLOW)
+reset_btn.grid(column=2, row=2)
+
+# Checkmark label
+check_label = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
+
+window.mainloop()
